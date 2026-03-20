8a50000032a06f71a68bbce09e7ed3d991d4e5a3
mperepelitsa23 <chbeeb23@gmail.com>
2026-03-20 22:51:22 +0200

    NixHomework2 initial commit

---
+++ Source/NixHomework2/NixHomework2.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "NixHomework2.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, NixHomework2, "NixHomework2" );
